54e212fc630a64f74cc64d0b512967dedb328b06
Pipi <ducanhyh5a@gmail.com>
2026-01-01 22:51:52 +0700

    Update LoadConfig.cpp

--- LoadConfig.cpp
+++ LoadConfig.cpp
@@ -1,26 +1,22 @@
+#include "Config.h"
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Config.h"
-#include "json.hpp" // Thư viện xử lý JSON
 
-using json = nlohmann::json;
-using namespace std;
+Config loadConfig(const std::string& filePath) {
+    std::ifstream file(filePath);
+    
+    if (!file.is_open()) {
+        std::cerr << "[Loi Module 1] Khong tim thay file config: " << filePath << std::endl;
+        return Config(); 
+    }
 
-Config loadConfig(const string& filePath) {
-    ifstream file(filePath);
     json j;
-    file >> j; // Đọc nội dung file vào biến json
+    file >> j; // Đọc toàn bộ file vào biến json
 
-    Config cfg;
-    cfg.simulation_name = j["simulation_name"];
-    cfg.total_time = j["total_time"];
-    cfg.bandwidth_mbps = j["bandwidth_mbps"];
-    cfg.max_queue_size = j["max_queue_size"];
-    cfg.traffic_rate.normal = j["traffic_rate"]["normal"];
-    cfg.traffic_rate.vip = j["traffic_rate"]["vip"];
+    Config cfg = j.get<Config>(); 
 
+    std::cout << "[Module 1] Da load config: " << cfg.simulation_name << std::endl;
+    
     return cfg;
 }
-
-
